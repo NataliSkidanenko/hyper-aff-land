f6abee0d12c4aef23315b7b68baa15c989d403e0
NataliSkidanenko <sni.nata102@gmail.com>
2023-10-08 19:59:32 +0300

    Add precached media

--- src/js/grid-animation.js
+++ src/js/grid-animation.js
@@ -1,5 +1,6 @@
 const grid = document.getElementById('grid');
-if (document.body.scrollWidth > 1000) {
+
+if (grid && document.body.scrollWidth > 1000) {
   document.addEventListener('mousemove', (e) => {
     grid.style.top = `${document.body.scrollWidth - e.clientY - grid.clientWidth / 2}px`;
     grid.style.left = `${document.body.scrollWidth - e.clientX - grid.clientWidth / 2}px`;
